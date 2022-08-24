99df5909f0fce8be6b4b3c3bec9c1af2cce3a40c
Jahiker <rojasjahiker@gmail.com>
2022-08-24 00:27:09 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/chats">Chats</router-link>
+  </div>
+  <router-view>
+
+  </router-view>
+</template>
+
+<style scoped>
+</style>
